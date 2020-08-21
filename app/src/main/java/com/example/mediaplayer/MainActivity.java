7a71d1c30006cf4fd82dff5ada2bd33cd3493904
user <Deepakshi.deeps@gmail.com>
2020-08-21 18:19:16 +0530

    Media Player

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,"Paused",Toast.LENGTH_SHORT).show();
             mediaPlayer.pause();
-            mediaPlayer.seekTo(5);
+
             }
         });
     }
